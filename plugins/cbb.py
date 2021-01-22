3280567a8e821493e287ca42af52ea2054d941d4
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2021-01-22 23:03:08 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -248,20 +248,20 @@ async def cb_handler(client, query):
     elif query.data == "start_data":
 
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("HELP", callback_data="help_data"), InlineKeyboardButton("ABOUT", callback_data="about_data"), ],
-                                         [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor")]])
+                                         [InlineKeyboardButton("CREATOR", url="https://t.me/imjerin")]])
 
         await query.message.edit_text(script.START_MSG.format(query.from_user.mention), reply_markup=keyboard, disable_web_page_preview=True)
 
     elif query.data == "help_data":
 
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("BACK", callback_data="start_data"), InlineKeyboardButton("ABOUT", callback_data="about_data"), ],
-                                         [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor")]])
+                                         [InlineKeyboardButton("CREATOR", url="https://t.me/imjerin")]])
 
         await query.message.edit_text(script.HELP_MSG, reply_markup=keyboard, disable_web_page_preview=True)
 
     elif query.data == "about_data":
 
         keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("BACK", callback_data="help_data"), InlineKeyboardButton("START", callback_data="start_data"), ],
-                                         [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Image-Editor")]])
+                                         [InlineKeyboardButton("CREATOR", url="https://t.me/imjerin")]])
 
         await query.message.edit_text(script.ABOUT_MSG, reply_markup=keyboard, disable_web_page_preview=True)
